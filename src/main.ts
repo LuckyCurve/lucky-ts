494b8c4a9ac72143781e4aef72088a8fa1bc1369
LuckyCurve <luckycurvec@gmail.com>
2025-08-09 09:30:31 +0800

    feat: update version retrieval in CLI to use package.json

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@
 
 import { Command } from "commander";
 import { ssh } from "./service/ssh.js";
+import pkg from "../package.json" with { type: "json" };
 
 const program: Command = new Command();
 
 program
     .name("lucky-ts")
     .description("CLI to some common utilities")
-    .version("0.0.3");
+    .version(pkg.version);
 
 program
     .command("ssh <target>")
